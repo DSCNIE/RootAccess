6bb0a7b25723ff5dd6cc6879984dab8e2d40a3f6
jatin <jatinmahajan1713@gmail.com>
2024-01-05 18:59:53 +0530

    initial commit

---
+++ WellnestAi/server/mainFolder/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('getResponse/',views.Chatbot.as_view(),name='getResponse'),
+    path('home/',views.home,name="HomePage")
+]
